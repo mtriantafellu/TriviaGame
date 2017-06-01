202c4fdcaa8fc7d19a624906f3ffe1b235452dde
matt <mtriantafellu@yahoo.com>
2017-05-31 22:03:29 -0400

    Start button working, revealing 3 panels upon click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,10 @@
 //on.Click display timer at top of panel - timer starts when start clicked
 //on.Click display reset button at bottom - resets entire page
 //Four questions with four possible answers
+//Create an object with questions and answers
+//Slot 0 for Questions, Slot 1-4 for answers
+//Pull slot 0 post in top
+//Pull slot 1-4 post below with 4 buttons
 
 //How to Show/Hide IDs:
 /* 
@@ -21,6 +25,18 @@ To hide it: $("#myId").addClass('hidden');
 To toggle it: $("#myId").toggleClass('hidden'); 
 */
 
+$(document).ready(function() {
+
 $('#panelText').addClass('hidden');
 $('#timer').addClass('hidden');
 $('#resetBtn').addClass('hidden');
+
+$("#startBtn").on("click", function() {
+
+	$('#timer').removeClass('hidden')
+	$('#panelText').removeClass('hidden')
+	$('#resetBtn').removeClass('hidden')
+
+	});	// End startBtn
+
+}); // End document.ready
