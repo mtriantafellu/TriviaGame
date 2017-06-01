208f664bff8a8db1c66dc06689acf8d0aaa57d0a
matt <mtriantafellu@yahoo.com>
2017-05-31 21:18:30 -0400

    Initial website with assets hooked up and verified working

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//Buttons:
+//  startBtn
+//  resetBtn
+
+//IDs:
+//  panelText
+//  timer
+
+//PseudoCode:
+
+//User clicks button
+//on.Click display questions in panel
+//on.Click display timer at top of panel - timer starts when start clicked
+//on.Click display reset button at bottom - resets entire page
+//Four questions with four possible answers
+
+//How to Show/Hide IDs:
+/* 
+To show it: $("#myId").removeClass('hidden');
+To hide it: $("#myId").addClass('hidden');
+To toggle it: $("#myId").toggleClass('hidden'); 
+*/
+
+$('#panelText').addClass('hidden');
+$('#timer').addClass('hidden');
+$('#resetBtn').addClass('hidden');
