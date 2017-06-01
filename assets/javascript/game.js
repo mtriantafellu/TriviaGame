54a0d6fe82cacead6a530c6f0fe2b6bbfa714786
matt <mtriantafellu@yahoo.com>
2017-05-31 23:51:36 -0400

    timer and buttons working...kinda

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,29 +14,110 @@
 //on.Click display reset button at bottom - resets entire page
 //Four questions with four possible answers
 //Create an object with questions and answers
+  //  Create an array of questions
+  //  Create an array of answers
+  //  Create an array of filler options
 //Slot 0 for Questions, Slot 1-4 for answers
 //Pull slot 0 post in top
 //Pull slot 1-4 post below with 4 buttons
 
-//How to Show/Hide IDs:
-/* 
-To show it: $("#myId").removeClass('hidden');
-To hide it: $("#myId").addClass('hidden');
-To toggle it: $("#myId").toggleClass('hidden'); 
-*/
-
 $(document).ready(function() {
 
-$('#panelText').addClass('hidden');
-$('#timer').addClass('hidden');
-$('#resetBtn').addClass('hidden');
+//When window is first loaded:
+window.onload = function() {
+};
 
-$("#startBtn").on("click", function() {
+//on.Click startBtn reveal three panels and start timer
+$('#startBtn').on("click", function() {
 
-	$('#timer').removeClass('hidden')
-	$('#panelText').removeClass('hidden')
-	$('#resetBtn').removeClass('hidden')
+	$('#wrapper').removeClass('hidden');
+	$('#startBtn').addClass('hidden');
 
-	});	// End startBtn
+});	// End startBtn
+
+//on.Click resetBtn hide three panels
+$("#resetBtn").on("click", function() {
+
+	$('#wrapper').addClass('hidden');
+	$('#startBtn').removeClass('hidden');
+
+}); // End resetBtn	
+
+//  Timer Function Starts:
+window.onload = function() {
+  $("#startBtn").on("click", stopwatch.start);
+  $("#resetBtn").on("click", stopwatch.reset);
+};
+
+var intervalId;
+var clockRunning = false;
+var stopwatch = {
+	//initial time = 10seconds
+	time: 10,
+
+	//Start
+	start: function() {
+
+		// DONE: Use setInterval to start the count here and set the clock to running.
+	    if (!clockRunning) {
+	        intervalId = setInterval(stopwatch.count, 1000);
+	        clockRunning = true;
+	    }
+	},  // End Start
+
+	//Reset
+	reset: function() {
+		clearInterval(intervalId);
+		clockRunning = false;
+		stopwatch.time = 10;
+		$('display').html("00:10");
+	    
+	    //Restart
+	    intervalId = setInterval(stopwatch.count, 1000);
+	    clockRunning = false;
+
+	// DONE: Change the "display" div to "00:00."
+	$("#display").html("00:10");
+
+	},  // End Reset
+	
+	//Counter	
+	count: function() {
+		//Decrement stopwatch time by 1
+	    stopwatch.time--;
+	    var converted = stopwatch.timeConverter(stopwatch.time);
+	    console.log(converted);
+
+	    //Display in HTML
+	    $("#display").html(converted);
+	
+	}, // End Count
+	
+	timeConverter: function(t) {
+
+	    var minutes = Math.floor(t / 60);
+	    var seconds = t - (minutes * 60);
+
+		    if (seconds < 10) {
+		      seconds = "0" + seconds;
+		    }
+
+		    if (minutes === 0) {
+		      minutes = "00";
+		    }
+		    else if (minutes < 10) {
+		      minutes = "0" + minutes;
+		    }
+
+		    if (seconds == 0) {
+		    	clearInterval(intervalId);
+		    	clockRunning === false;
+		    }
+
+		    return minutes + ":" + seconds;
+	  	}
+
+}; // End Timer
+
 
 }); // End document.ready
